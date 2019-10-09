c72f8fb8b9d5dfd1abfc745aaabfee9da4176e16
Kellymsmith7 <kellymsmith7@gmail.com>
2019-10-08 20:45:26 -0500

    Failed Basic List Exception

--- test_collections/test_basic_list_exception.py
+++ test_collections/test_basic_list_exception.py
@@ -1,9 +1,17 @@
 import unittest
+from unittest.mock import patch
+import fun_with_collections.basic_list as topic1
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class TestList(unittest.TestCase):
+    @patch('fun_with_collections.basic_list.get_input', return_value='7')
+    def test_make_list(self, input):
+        self.assertEqual(topic1.make_list(), ['7', '7', '7'])
+
+    @patch('fun_with_collections.basic_list.get_input', return_value='ab')
+    def test_make_list_non_numeric(self, input):
+        with self.assertRaises(ValueError):
+            topic1.make_list()
 
 
 if __name__ == '__main__':
