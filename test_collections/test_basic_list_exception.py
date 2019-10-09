b1b2bbad02e9c9dff31c5557d71340e5a2b713fb
Kellymsmith7 <kellymsmith7@gmail.com>
2019-10-08 22:32:19 -0500

    Pass Basic List Exception above and below range

--- test_collections/test_basic_list_exception.py
+++ test_collections/test_basic_list_exception.py
@@ -4,24 +4,14 @@ import fun_with_collections.basic_list_exception as topic1
 
 
 class TestList(unittest.TestCase):
-    @patch('fun_with_collections.basic_list_exception.get_input', return_value='7')
-    def test_make_list(self, input):
-        self.assertEqual(topic1.make_list(), ['7', '7', '7'])
-
-    @patch('fun_with_collections.basic_list_exception.get_input', return_value='ab')
-    def test_make_list_non_numeric(self, input):
-        with self.assertRaises(ValueError):
-            topic1.make_list()
-
     def test_below_range(self):
-        input_number = -1
-        expected_result = ValueError
-        self.assertEqual(ValueError, topic1.make_list(-1))
+        with self.assertRaises(ValueError):
+            topic1.get_input()
 
     def test_above_range(self):
-        input_number = 55
-        excepted_result = ValueError
-        self.assertEqual(ValueError, topic1.make_list(55))
+        with self.assertRaises(ValueError):
+            topic1.get_input()
+
 
 
 if __name__ == '__main__':
